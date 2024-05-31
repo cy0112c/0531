1351821e858010890ca395d97fd4cbc2c4fe5a36
Young <tmznt11@naver.com>
2024-05-31 13:15:36 +0900

    카운터 구현 논리 완성

--- src/app/assignments/counter/page.tsx
+++ src/app/assignments/counter/page.tsx
@@ -1,5 +1,30 @@
-"use client"
+"use client";
+
+import Header from "@/components/header";
+import { useState } from "react";
 
 export default function Assignment3() {
-  return <div>안녕하세요</div>;
+  return (
+    <>
+      <Header title={"카운터 만들기"} hasBack={true} />
+      <Counter></Counter>
+    </>
+  );
+}
+
+function Counter() {
+  let count = 0;
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          count = count + 1;
+          console.log(count);
+        }}
+      >
+        증가
+      </button>
+    </div>
+  );
 }
