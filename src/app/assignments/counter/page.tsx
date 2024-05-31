3b5422052e4b7fbeffa1337a155fbdc4c7c3f0a5
Young <tmznt11@naver.com>
2024-05-31 15:27:19 +0900

    카운터 구현

--- src/app/assignments/counter/page.tsx
+++ src/app/assignments/counter/page.tsx
@@ -15,18 +15,30 @@ export default function Assignment3() {
 function Counter() {
   // let count = 0;
   const [count, setCount] = useState(0);
+
   return (
-    <div>
-      <h1>{count}</h1>
-      <button
-        onClick={() => {
-          // count = count + 1;
-          setCount(count + 1);
-          console.log(count);
-        }}
-      >
-        증가
-      </button>
+    <div className="flex flex-col items-center gap-2 m-5">
+      <div className="text-6xl">{count}</div>
+      <div className="flex gap-2">
+        <button
+          className="bg-blue-400 text-white font-semibold text-2xl size-10 rounded-md active:bg-blue-500 active:scale-95"
+          onClick={() => {
+            // count = count + 1;
+            setCount(count + 1);
+          }}
+        >
+          +
+        </button>
+        <button
+          className="bg-blue-400 text-white font-semibold text-2xl size-10 rounded-md active:bg-blue-500 active:scale-95"
+          onClick={() => {
+            if(count >0) {
+            setCount(count - 1)};
+          }}
+        >
+          -
+        </button>
+      </div>
     </div>
   );
 }
